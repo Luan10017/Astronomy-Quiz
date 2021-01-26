9ef122c5a8718cb1fbedd633cdb62453dcbef89b
Luan10017 <32424296+Luan10017@users.noreply.github.com>
2021-01-26 14:31:46 -0300

    Arrumando layout do quiz

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-export default function PudimComBatata() {
+export default function Quiz() {
   return (
-    <div>
-      Página de quiz
-    </div>
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Astronomia</title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Pergunta 1 de 5</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <img src={db.questions[0].image} alt=""/>  
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+            }}
+            >
+              <button type="submit">
+                Confirmado 
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        
+      </QuizContainer>
+    </QuizBackground>
   );
 }
